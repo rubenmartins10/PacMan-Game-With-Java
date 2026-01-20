2690bff28f3a785cede23cbb80b8fe8244aeba0c
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2026-01-20 18:04:29 +0000

    Terceira atualização

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 import javax.swing.JFrame;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
         int rowCount = 21;
@@ -8,12 +7,19 @@ public class App {
         int tileSize = 32;
         int boardWidth = columnCount * tileSize;
         int boardHeight = rowCount * tileSize;
+        
 
         JFrame frame = new JFrame("PacMan"); //título da janela
-        frame.setVisible(true); //tornar a janela visível
+        //frame.setVisible(true); //tornar a janela visível
         frame.setSize(boardWidth, boardHeight); //definir o tamanho da janela
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //fechar a aplicação ao carregar no X
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
