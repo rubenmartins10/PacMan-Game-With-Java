4a7a5df49a7ed2202877705caf09719e9ce59de9
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2026-01-20 17:26:26 +0000

    Janela vazia

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import javax.swing.JFrame;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("PacMan"); //título da janela
+        frame.setVisible(true); //tornar a janela visível
+        frame.setSize(boardWidth, boardHeight); //definir o tamanho da janela
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //fechar a aplicação ao carregar no X
     }
 }
